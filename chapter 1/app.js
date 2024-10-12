1eb163ba2363352ee4eb5bbcaecbfad8d5598e14
Anamta2626 <anamtasajid7272@gmail.com>
2024-10-12 21:43:02 +0500

    vs code

---
+++ chapter 1/app.js
@@ -0,0 +1,31 @@
+// ............Question no 01............
+// alert("Welcome to our website! We're glad to have you here.");
+
+
+
+// ...........Question no 02..........
+// alert("Error! Please enter a valid password.");
+
+
+// .............Qyestion no 03...........
+// alert("Welcome to JS Land... \nHappy Coding!");
+
+
+// .............Qyestion no 04...........
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \nPrevent this page from creating additional dialogs.")
+
+
+// .............Qyestion no 05...........
+// alert("Hello...! I can run JS through my web browser's console")
+
+
+// .............Qyestion no 06...........
+alert("Welcome to the login page!")
+
+
+
+
+
+
+
