b8ae472c70062a88a6175863c00f35c3f764f14d
Anamta2626 <anamtasajid7272@gmail.com>
2024-10-12 21:43:31 +0500

    chapter 3

---
+++ chapter 3/app.js
@@ -0,0 +1,29 @@
+// ...........Question 01...........
+// let age = 21;
+// alert('I am ' + age + ' years old.');
+
+
+// ...........Question 02...........
+// if (localStorage.getItem('visitCount')) {
+//     let visitCount = parseInt(localStorage.getItem('visitCount')) + 1;
+//     localStorage.setItem('visitCount', visitCount);
+//     alert("You have visited this site " + visitCount + " times");
+//   } else {
+//     localStorage.setItem('visitCount', 1);
+//     alert("You have visited this site 1 time");
+//   }
+  
+
+
+// ...........Question 03...........
+// let birthYear = 2002;
+// let message = 'Datatype of my declared variable is ';
+// document.write('My birth year is ' + birthYear + "<br>" + message + typeof birthYear);
+
+
+
+// ...........Question 04...........
+// let visitorsName = 'Anamta';
+// let product = 'kurtis';
+// let quantity = 3;
+// document.write(visitorsName + ' ordered ' + quantity + " " + product + ' on XYZ clothing store.')
