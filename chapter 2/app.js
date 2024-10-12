a5eb55d38cd5834cf2793a6ac912144b05bacf92
Anamta2626 <anamtasajid7272@gmail.com>
2024-10-12 21:43:19 +0500

    chapter 2

---
+++ chapter 2/app.js
@@ -0,0 +1,52 @@
+// ...........Question 01...........
+// let username = "anam_7272";
+// console.log(username);
+
+
+// ...........Question 02...........
+// let myName = "Anamta Sajid Ali";
+// console.log(myName);
+
+
+// ...........Question 03...........
+// let message = "Hello World!";
+// alert(message);
+
+
+// ...........Question 04...........
+// let studentName = "Anamta Sajid Ali";
+// let studentAge = 21;
+// let studentSkill = "Web developer";
+// alert(studentName);
+// alert(studentAge + " years old.");
+// alert(studentSkill);
+
+
+// ...........Question 05...........
+// let word = "PIZZA";
+// let message = "";
+
+// for (let i = word.length; i > 0; i--) {
+//   message += word.slice(0, i) + "\n";
+// }
+
+// alert(message);
+
+
+// ...........Question 06...........
+// let email = "anamtasajid727@gmail.com";
+// alert("My email address is " + email);
+
+
+// ...........Question 07...........
+// let book = "A smarter way to learn javascript";
+// alert(book);
+
+
+// ...........Question 08...........
+// document.write(<h1>Yah! I can write HTML content through Javascript.</h1>)
+
+
+// ...........Question 09...........
+
+
