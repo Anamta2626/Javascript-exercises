16c743948b9fbd84a26bf946c6fbdaa11563a573
Anamta2626 <anamtasajid7272@gmail.com>
2024-10-12 21:43:57 +0500

    chapter 4

---
+++ chapter 4/app.js
@@ -0,0 +1,23 @@
+// ...........Question 01...........
+// let name= 'Anamta', age = 21 , city = 'karachi';
+
+// ...........Question 02...........
+// 5 Legal Variable names
+// let firstName = "Anamta";
+// let _name = 'Anamta';
+// let $salary = "30k";
+// let number12 = 30;
+// let last_name = 'Sajid ali';
+
+// 5 illegal variables names
+// let 2first_name = 'Anamta';
+// let @age = 21;
+// let last name = "sajid ali";
+// let var = 'hey';
+// let first-name = 'anamta';
+
+
+// ...........Question 03...........
+
+
+
